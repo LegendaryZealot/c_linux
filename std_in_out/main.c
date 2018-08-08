0553b306997182c7e9de7055d85b1b169132ba25
LegendaryZealot <875626439@qq.com>
2018-08-08 16:55:29 +0800

    add fgets from stdin

--- std_in_out/main.c
+++ std_in_out/main.c
@@ -4,16 +4,23 @@
 
 int main(int argc, char const *argv[])
 {
-    int c;
-    while(EOF!=(c=getc(stdin)))
+    //old version
+    // int c;
+    // while(EOF!=(c=getc(stdin)))
+    // {
+    //     printf("enter while\n");
+    //     if(EOF==putc(c,stdout))
+    //     {
+    //         printf("error\n");
+    //         break;
+    //     }
+    //     printf("print done\n");
+    // }
+    char buf[128];
+    while(NULL!=fgets(buf,sizeof(buf),stdin))
     {
-        printf("enter while\n");
-        if(EOF==putc(c,stdout))
-        {
-            printf("error\n");
-            break;
-        }
-        printf("print done\n");
+        printf("%s",buf);
     }
+    printf("\n");
     return 0;
 }
