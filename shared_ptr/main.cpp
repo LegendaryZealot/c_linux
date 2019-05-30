b78ae408e6fed3670c968e5a77311e71b2760ad1
李桩 <lizhuang@lizhuangdeMacBook-Pro.local>
2019-05-31 00:28:39 +0800

    add shared_ptr

---
+++ shared_ptr/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class CConnnect
+{
+public:
+    void Disconnect() { 
+        cout<<"Disconnect"<<endl;
+    }
+    ~CConnnect(){
+        cout<<"~CConnnect"<<endl;
+    }
+};
+
+void Deleter(CConnnect* obj)
+{
+    obj->Disconnect(); // 做其它释放或断开连接等工作
+    delete obj; // 删除对象指针
+}
+
+int main(int argc,char *argv[]){
+    shared_ptr<CConnnect> sps_1(new CConnnect, Deleter);
+    shared_ptr<CConnnect> sps_2(sps_1);
+    std::cout << "sps_1 Reference count = " << sps_1.use_count() << std::endl;
+	std::cout << "sps_2 Reference count = " << sps_2.use_count() << std::endl;
+    sps_1.reset();
+    std::cout << "sps_1 Reference count = " << sps_1.use_count() << std::endl;
+	std::cout << "sps_2 Reference count = " << sps_2.use_count() << std::endl;
+    sps_2.reset();
+    std::cout << "sps_1 Reference count = " << sps_1.use_count() << std::endl;
+	std::cout << "sps_2 Reference count = " << sps_2.use_count() << std::endl;
+    return 0;
+}
