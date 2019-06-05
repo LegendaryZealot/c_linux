eac318c38ee4eb946064fbc3c767593c50956858
李桩 <lizhuang@lizhuangdeMacBook-Pro.local>
2019-06-05 20:52:23 +0800

    modify main.cpp

--- shared_ptr/main.cpp
+++ shared_ptr/main.cpp
@@ -19,8 +19,12 @@ void Deleter(CConnnect* obj)
 }
 
 int main(int argc,char *argv[]){
-    shared_ptr<CConnnect> sps_1(new CConnnect, Deleter);
+    shared_ptr<CConnnect> sps_1(new CConnnect);
+    // shared_ptr<CConnnect> sps_1(new CConnnect, Deleter);//自定义删除
     shared_ptr<CConnnect> sps_2(sps_1);
+    std::cout << "sps_1 Reference count = " << sps_1.use_count() << std::endl;
+	std::cout << "sps_2 Reference count = " << sps_2.use_count() << std::endl;
+    weak_ptr<CConnnect> wps_q(sps_1);
     std::cout << "sps_1 Reference count = " << sps_1.use_count() << std::endl;
 	std::cout << "sps_2 Reference count = " << sps_2.use_count() << std::endl;
     sps_1.reset();
