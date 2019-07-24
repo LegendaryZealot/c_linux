2f2ef30daabf241ef303b05d495b9c55987aba9f
LegendaryZealot <875626439@qq.com>
2019-07-25 00:21:32 +0800

    add dlopen

---
+++ dlopen/caculate.c
@@ -0,0 +1,25 @@
+extern void test();
+
+int add(int a,int b)
+{
+    test();
+    return (a + b);
+}
+
+int sub(int a, int b)
+{
+    test();
+    return (a - b);
+}
+
+int mul(int a, int b)
+{
+    test();
+    return (a * b);
+}
+
+int div(int a, int b)
+{
+    test();
+    return (a / b);
+}
