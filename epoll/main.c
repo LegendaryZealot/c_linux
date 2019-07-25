f2f4aa22db797c23fbc97d982b026f5acc96f1e5
LegendaryZealot <875626439@qq.com>
2019-07-25 22:13:55 +0800

    fix epoll

--- epoll/main.c
+++ epoll/main.c
@@ -1,8 +1,3 @@
-/* 实现功能：通过epoll, 处理多个socket
- * 监听一个端口,监听到有链接时,添加到epoll_event
- * xs
- */
- 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,18 +6,23 @@
 #include <sys/epoll.h>
 #include <sys/time.h>
 #include <netinet/in.h>
+
+#include <sys/types.h>
+#include <unistd.h>
  
 #define MYPORT 12345
  
 //最多处理的connect
-#define MAX_EVENTS 500
+#define MAX_EVENTS 4096
  
 //当前的连接数
 int currentClient = 0; 
  
 //数据接受 buf
-#define REVLEN 1000
+#define REVLEN 128
 char recvBuf[REVLEN+1];
+
+int handledFd=0;
  
  
 //epoll描述符
@@ -32,9 +32,14 @@ struct epoll_event eventList[MAX_EVENTS];
  
 void AcceptConn(int srvfd);
 void RecvData(int fd);
+
+
+char *tmp="Content-Type:text/html\r\n\r\nhi,epoll\n";
  
 int main()
 {
+    printf("pid is:%d\n",getpid());
+
     int i, ret, sinSize;
     int recvLen = 0;
     fd_set readfds, writefds;
@@ -42,146 +47,121 @@ int main()
     int timeout;
     struct sockaddr_in server_addr;
     struct sockaddr_in client_addr;
-    
-    //socket
-    if((sockListen=socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
+    if(0>(sockListen=socket(AF_INET, SOCK_STREAM, 0))){
+        char error[128];
+        perror(error);
+        printf("%s\n",error);
         printf("socket error\n");
         return -1;
     }
-    
     bzero(&server_addr, sizeof(server_addr));
     server_addr.sin_family  =  AF_INET;
     server_addr.sin_port = htons(MYPORT);
     server_addr.sin_addr.s_addr  =  htonl(INADDR_ANY); 
-    
-    //bind
-    if(bind(sockListen, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0)
-    {
+    if(0!=bind(sockListen, (struct sockaddr*)&server_addr, sizeof(server_addr))){
+        char error[128];
+        perror(error);
+        printf("%s\n",error);
         printf("bind error\n");
         return -1;
     }
-    
-    //listen
-    if(listen(sockListen, 5) < 0)
-    {
+    if(0!=listen(sockListen, 5)){
+        char error[128];
+        perror(error);
+        printf("%s\n",error);
         printf("listen error\n");
         return -1;
     }
     
-    // epoll 初始化
     epollfd = epoll_create(MAX_EVENTS);
     struct epoll_event event;
     event.events = EPOLLIN|EPOLLET;
     event.data.fd = sockListen;
-    
-    //add Event
-    if(epoll_ctl(epollfd, EPOLL_CTL_ADD, sockListen, &event) < 0)
-    {
+    if(0!=epoll_ctl(epollfd, EPOLL_CTL_ADD, sockListen, &event)){
+        char error[128];
+        perror(error);
+        printf("%s\n",error);
         printf("epoll add fail : fd = %d\n", sockListen);
         return -1;
     }
-    
-    //epoll
-    while(1)
-    {
-        timeout=3000;                
-        //epoll_wait
+    while(1){
+        timeout=3000;
         int ret = epoll_wait(epollfd, eventList, MAX_EVENTS, timeout);
-        
-        if(ret < 0)
-        {
-            printf("epoll error\n");
+        if(ret < 0){
+            char error[128];
+            perror(error);
+            printf("%s\n",error);
             break;
-        }
-        else if(ret == 0)
+        }else if(ret == 0)
         {
-            printf("timeout ...\n");
+            printf("timeout ...\r");
+            fflush(stdout);
             continue;
+        }else{        
+            printf("current link:%d\r",ret);
+            fflush(stdout);
         }
-        
-        //直接获取了事件数量,给出了活动的流,这里是和poll区别的关键
-        int i = 0;
-        for(i=0; i<ret; i++)
+        for(int i=0; i<ret; i++)
         {
-            //错误退出
             if ((eventList[i].events & EPOLLERR) ||
                 (eventList[i].events & EPOLLHUP) ||
                 !(eventList[i].events & EPOLLIN))
             {
               printf ( "epoll error\n");
               close (eventList[i].data.fd);
-              return -1;
+              continue;
             }
-            
             if (eventList[i].data.fd == sockListen)
             {
                 AcceptConn(sockListen);
         
             }else{
+                struct epoll_event event;
+                event.data.fd = eventList[i].data.fd;
+                event.events =  EPOLLIN|EPOLLET;
+                epoll_ctl(epollfd, EPOLL_CTL_DEL, eventList[i].data.fd, &event);
                 RecvData(eventList[i].data.fd);
             }
         }
     }
-    
-    close(epollfd);
     close(sockListen);
-    
- 
+    close(epollfd);
     return 0;
 }
- 
-/**************************************************
-函数名：AcceptConn
-功能：接受客户端的链接
-参数：srvfd：监听SOCKET
-***************************************************/
+
 void AcceptConn(int srvfd)
 {
     struct sockaddr_in sin;
     socklen_t len = sizeof(struct sockaddr_in);
     bzero(&sin, len);
- 
     int confd = accept(srvfd, (struct sockaddr*)&sin, &len);
- 
-    if (confd < 0)
-    {
+    if (confd < 0){
        printf("bad accept\n");
        return;
-    }else
-    {
-        printf("Accept Connection: %d\n", confd);
     }
-    //将新建立的连接添加到EPOLL的监听中
     struct epoll_event event;
     event.data.fd = confd;
     event.events =  EPOLLIN|EPOLLET;
     epoll_ctl(epollfd, EPOLL_CTL_ADD, confd, &event);
 }
- 
-//读取数据
+
 void RecvData(int fd)
 {
     int ret;
     int recvLen = 0;
-    const int msgLen=10;
+    const int msgLen=16;
     memset(recvBuf, 0, REVLEN);
-    printf("RecvData function\n");
-    while (0<(ret=recv(fd, (char *)recvBuf+recvLen, msgLen, MSG_CMSG_CLOEXEC)))
-    {
-        if(0==ret){
-            struct epoll_event event;
-            event.data.fd = fd;
-            event.events =  EPOLLIN|EPOLLET;
-            epoll_ctl(epollfd, EPOLL_CTL_DEL, fd, &event);
-        }else if(0>ret){
-            printf("socket fd read error code:%d\n",ret);
-            return;
-        }else if(msgLen>ret){
-            close(fd);
+    while (0<(ret=read(fd, (char *)recvBuf+recvLen, msgLen))){
+        recvLen+=ret;
+        if(msgLen>ret){
             break;
         }
-        recvLen+=ret;
+        if(msgLen>(REVLEN-recvLen)){
+            break;
+        }
     }
-    printf("data is:%s\n", recvBuf);
+    write(fd,recvBuf,strlen(recvBuf));
+    printf("handled socket fd:%d\r",++handledFd);
+    fflush(stdout);
+    close(fd);
 }
